90f2637a75f8eadd2319412d9350aa957dd8298f
Glenon Mateus <glenonmateus@gmail.com>
2019-03-14 11:30:58 -0300

    add exercicio file seção 7 add definindo_funções file

---
+++ seção7/exercicio.py
@@ -0,0 +1,31 @@
+"""
+# Exercicio 1
+A = [1, 0, 5, -2, -5, 7]
+soma = A[0] + A[1] + A[5]
+print(A)
+print(f'A soma dos valores {soma}')
+A[3] = 100
+for i in A:
+    print(i)
+
+# Exercicio 2
+lista = []
+for i in range(6):
+    entrada = int(input('Digite um número: '))
+    lista.append(entrada)
+print(lista)
+
+# Exercicio 3
+lista = []
+lista_quadrado = []
+for i in range(10):
+    entrada = int(input('Digite um número: '))
+    lista.append(entrada)
+for i in lista:
+    lista_quadrado.append(i ** 2)
+print(lista)
+print(lista_quadrado)
+"""
+
+# Exercicio 4
+
