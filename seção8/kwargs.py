54c98f851978c41e679380b2563571a33d266248
Glenon Mateus <glenonmateus@gmail.com>
2019-03-24 13:51:00 -0300

    add kwargs information

--- seção8/kwargs.py
+++ seção8/kwargs.py
@@ -39,4 +39,75 @@ print(cumprimento_especial())
 print(cumprimento_especial(geek='Python'))
 print(cumprimento_especial(geek='Oi'))
 print(cumprimento_especial(geek='especial'))
+
+# Nas nossas funções, podemos ter (nesta ordem):
+# - parâmetros obrigatórios;
+#   -*args;
+# - parâmetros default (não obrigatórios);
+#   -**kwargs
+
+
+def minha_funcao(idade, nome, *args, solteiro=False, **kwargs):
+    print(f'{nome} tem {idade} anos')
+    print(args)
+    if solteiro:
+        print('Solteiro')
+    else:
+        print('Casado')
+    print(kwargs)
+
+
+minha_funcao(8, 'Julia')
+minha_funcao(18, 'Felicity', 4, 5, 3, solteiro=True)
+minha_funcao(34, 'Felipe', eu='Não', voce='Vai')
+minha_funcao(19, 'Carla', 9, 4, 3, java=False, python=True)
+
+# Entenda por quê é importante manter a ordem dos parâmetros na declaração
+
+# Função com a ordem corretade parâmetros
+# def mostra_info(a, b, *args, instrutor='Geek', **kwargs):
+#    return [a, b, args, instrutor, kwargs]
+
+# Função com a ordem incorreta de parâmetros
+
+
+def mostra_info(a, b, instrutor='Geek', *args, **kwargs):
+    return [a, b, args, instrutor, kwargs]
+
+
+print(mostra_info(1, 2, 3, sobrenome='University', cargo='instrutor'))
+
+# Desempacotar com **kwargs
+
+
+def mostra_nomes(**kwargs):
+    return f"{kwargs['nome']} {kwargs['sobrenome']}"
+
+
+nomes = {'nome': 'Felicity', 'sobrenome': 'Jones'}
+print(mostra_nomes(**nomes))
 """
+
+
+def soma_multiplos_numeros(a, b, c, **kwargs):
+    print(a + b + c)
+
+
+lista = [1, 2, 3]
+tupla = (1, 2, 3)
+conjunto = {1, 2, 3}
+dicionario = dict(a=1, b=2, c=3)
+
+soma_multiplos_numeros(*lista)
+soma_multiplos_numeros(*tupla)
+soma_multiplos_numeros(*conjunto)
+soma_multiplos_numeros(**dicionario)
+
+# OBS: Os nomes da chave em um dicionário devem ser os mesmo
+# dos parâmetros da função
+
+# dicionario = dict(d=1, e=2, f=3) # TypeError()
+# soma_multiplos_numeros(**dicionario)
+
+dicionario = dict(a=1, b=2, c=3, nome='Geek')
+soma_multiplos_numeros(**dicionario, lang='Python')
