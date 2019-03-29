070c67090a36c757cfc201c1a2d84cbbcb11a45e
Glenon Mateus <glenonmateus@gmail.com>
2019-03-29 08:23:40 -0300

    add sorted.py file

---
+++ seção10/sorted.py
@@ -0,0 +1,60 @@
+"""
+Sorted
+
+OBS: Não confunda, apesar do nome, com a função sort() que já estudamos em
+Listas. O sort() só funciona em listas.
+
+Podemos utilizar o sorted() com qualquer iterável.
+
+Como o próprio nome diz, sorted() server para ordenar.
+
+OBS: O sorted, SEMPRE retorna uma Lista com os elementos do iterável ordenados
+
+# Exemplo
+numeros = (6, 1, 8, 2)
+print(numeros)
+print(sorted(numeros))  # Ordernar do menos para o maior
+print(numeros)
+
+numeros = [6, 1, 8, 2]
+print(numeros)
+print(sorted(numeros))
+
+# Adicionando parâmetros ao sorted
+print(sorted(numeros, reverse=True))  # Ordena do maior para o menor
+
+# Podemos utilizar o sorted() para coisas mais complexas
+
+usuarios = [
+    {"username": "samuel", "tweets": ["Eu adodo bolos", "Eu adoro pizzas"]},
+    {"username": "carla", "tweets": ["Eu amo meu gato"]},
+    {"username": "jeff", "tweets": []},
+    {"username": "bob123", "tweets": []},
+    {"username": "doggo", "tweets": ["Eu gosto de cachorros",
+                                     "Vou sair hoje"]},
+    {"username": "gal", "tweets": []},
+]
+
+print(usuarios)
+
+# Ordenando pelo username - Ordem Alfabética
+print(sorted(usuarios, key=lambda usuario: usuario['username']))
+
+# Ordenando pelo número de tweets
+print(sorted(usuarios, key=lambda usuario: len(usuario['tweets'])))
+"""
+
+# Exemplo
+
+musicas = [
+    {'titulo': 'Thunderstruck', 'tocou': 3},
+    {'titulo': 'Dead Skin Mask', 'tocou': 2},
+    {'titulo': 'Back in Black', 'tocou': 4},
+    {'titulo': 'Too old to rock in roll, too ynoung to die', 'tocou': 32}
+]
+
+# Ordena da menos tocada para a mais tocada
+print(sorted(musicas, key=lambda musica: musica['tocou']))
+
+# Ordena da mais tocada para menos tocada
+print(sorted(musicas, key=lambda musica: musica['tocou'], reverse=True))
