786cf714f6dab72d8645f284273cc1a6c72cedc8
Glenon Mateus <glenonmateus@gmail.com>
2019-04-18 13:14:46 -0300

    add classes.py file

---
+++ secao16/classes.py
@@ -0,0 +1,53 @@
+"""
+POO - Classes
+
+Em POO, Classes nada mais são que modelos dos objetos do mundo real sendo
+representados computacionalmente.
+
+Imagine que você queira fazer um sistema para automatizar o controle das
+lâmpadas da sua casa.
+
+Classes podem conter:
+    - Atributos -> representam as características do objeto. Ou seja, pelos
+    atributos conseguimos representar computacionalmente os estados de um
+    objeto. No caso da lâmpada, possivelmente iriamos querer saber se a
+    lâmpada é 110 ou 220 volts, se ela é branca, amarela, vermelha ou outra
+    cor, qual é a luminosidade dela e etc;
+
+    - Métodos (funções) -> representam os comportamentos do objeto. Ou seja,
+    as ações que este objeto pode realizar no seu sistema. No caso da lâmpa-
+    da, por exemlo, um comportamento comum que muito provavelmente iriamos
+    querer representar no nosso sistema é o de ligar e desligar a mesma.
+
+Em Python, para definir um classe utilizamos a palavra reservada class.
+
+OBS: utilizamos a palavra pass em Python quando temos um bloco de código que
+ainda não está implementado.
+
+OBS: Quando nomeamos nossas classes em Python utilizamos por convenção o nome
+com inicial em maiúsculo. Se o nome for composto, utiliza-se as iniciais de
+ambas as palavras em maiúsculo, todas juntas.
+
+Dica: Em computação não utilizamos: Acentuação, carateres especiais, espaços ou
+similares para nomes de classes, atributos, métodos, arquivos, diretórios e etc
+
+OBS: quando estamos plenejando um software e definimos quais classes teremos
+que ter no sistema, chamamos estes objetos que serão mapeados para classes de
+entidade. 
+"""
+
+
+class Lampada:
+    pass
+
+
+class ContaCorrente:
+    pass
+
+
+class Produto:
+    pass
+
+
+class Usuario:
+    pass
