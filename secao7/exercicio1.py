b1588d045b456a1a364de4e45ac40577b3379f56
Glenon Mateus <glenonmateus@gmail.com>
2019-04-22 21:11:27 -0300

    rename file

---
+++ secao7/exercicio1.py
@@ -0,0 +1,255 @@
+"""
+# Exercicio 1
+A = [1, 0, 5, -2, -5, 7]
+soma = A[0] + A[1] + A[5]
+print(A)
+print(f'A soma dos valores {soma}')
+A[3] = 100
+for i in A:
+    print(i)
+
+# Exercicio 2
+lista = []
+for i in range(6):
+    entrada = int(input('Digite um número: '))
+    lista.append(entrada)
+print(lista)
+
+# Exercicio 3
+lista = []
+lista_quadrado = []
+for i in range(10):
+    entrada = int(input('Digite um número: '))
+    lista.append(entrada)
+for i in lista:
+    lista_quadrado.append(i ** 2)
+print(lista)
+print(lista_quadrado)
+
+# Exercicio 4
+lista = []
+for i in range(8):
+    entrada = float(input('Digite um número: '))
+    lista.append(entrada)
+posicao1 = int(input('Digite a primeira posição da lista: '))
+if posicao1 > 8 or posicao1 < 0:
+    print('Posição inválida.')
+    quit()
+posicao2 = int(input('Digite a segunda posição da lista: '))
+if posicao2 > 8 or posicao2 < 0:
+    print('Posição inválida.')
+    quit()
+soma = lista[posicao1] + lista[posicao2]
+print(f'A soma dos valores das posições escolhidas é {soma}')
+
+# Exercicio 8
+lista = []
+for i in range(6):
+    entrada = int(input('Digite um valor inteiro: '))
+    lista.append(entrada)
+print(lista[::-1])
+lista.reverse()
+print(lista)
+
+# Exercicio 10
+from random import randrange
+notas = []
+for i in range(15):
+    nota = randrange(10)
+    print(f'a nota do aluno {i} é {nota}')
+    notas.append(nota)
+print(f'A média geral das notas {sum(notas) / 15}')
+
+# Exercicio 21
+from random import randrange
+listaA = []
+listaB = []
+listaC = []
+for i in range(10):
+    listaA.append(randrange(100))
+    listaB.append(randrange(100))
+    listaC.append(listaA[i] - listaB[i])
+print(listaA)
+print(listaB)
+print(listaC)
+
+# Exercicio 20
+from random import randrange
+lista1 = []
+lista2 = []
+for i in range(10):
+    lista1.append(randrange(0, 50))
+    if lista1[i] % 2 != 0:
+        lista2.append(lista1[i])
+for i in range(len(lista1)):
+    print(lista1[i], end=' ')
+    if lista1.index(lista1[i]) % 2 != 0:
+        print()
+print()
+for i in range(len(lista2)):
+    print(lista2[i], end=' ')
+    if lista2.index(lista2[i]) % 2 != 0:
+        print()
+
+# Exercicio 26
+from random import randrange
+from math import sqrt
+
+lista = []
+for i in range(10):
+    lista.append(randrange(0, 50))
+print(lista)
+
+media = sum(lista) / len(lista)
+print(f'A média da amostra de dados é {media}')
+
+somatorio = 0
+for i in range(len(lista)):
+    somatorio += (lista[i] - media) ** 2
+print(f'O somatório é {somatorio}')
+
+desvio_padrao = sqrt(somatorio / len(lista))
+print(f'O desvio padrão do conjunto de dados é {desvio_padrao}')
+
+# Exercicio 27
+from random import randrange
+lista = []
+for i in range(10):
+    lista.append(randrange(0, 50))
+print(lista)
+
+primo = True
+for i in range(len(lista)):
+    for j in range(2, lista[i]):
+        if lista[i] % j == 0:
+            primo = False
+            break
+    if primo:
+        print(f'O elemento {lista[i]} é primo e está na posição {i}')
+    else:
+        print(f'O elemento {lista[i]} não é primo e está na posição {i}!')
+    primo = True
+
+# Exercicio 28
+from random import randrange
+v = []
+v1 = []
+v2 = []
+for i in range(10):
+    v.append(randrange(0, 50))
+print(v)
+
+for i in range(len(v)):
+    if v[i] % 2 == 0:
+        v1.append(v[i])
+    else:
+        v2.append(v[i])
+print(f'Os valores pares do vetor v é \n{v1}')
+print(f'Os valores ímpares do vetor v é \n{v2}')
+
+# Exercicio 30
+from random import randrange
+conjunto1 = set()
+conjunto2 = set()
+
+for i in range(10):
+    conjunto1.add(randrange(0, 50))
+    conjunto2.add(randrange(0, 50))
+print(conjunto1)
+print(conjunto2)
+print(conjunto1.intersection(conjunto2))
+
+# Exercicio 31
+from random import randrange
+v1 = set()
+v2 = set()
+for i in range(10):
+    v1.add(randrange(0, 50))
+    v2.add(randrange(0, 50))
+print(v1)
+print(v2)
+print(v1.union(v2))
+
+# Exercicio 32
+from random import randrange
+x = []
+y = []
+for i in range(5):
+    x.append(randrange(0, 50))
+    y.append(randrange(0, 50))
+print(f'x: {x}')
+print(f'y: {y}')
+
+print('Soma: ', end='')
+for i in range(len(x)):
+    print(x[i] + y[i], end=' ')
+
+print('\nMultiplicação: ', end='')
+for i in range(len(x)):
+    print(x[i] * y[i], end=' ')
+
+conjunto_x = set(x)
+conjunto_y = set(y)
+print(f'\nDiferença: {conjunto_x.difference(conjunto_y)}')
+print(f'Interseção: {conjunto_x.intersection(conjunto_y)}')
+print(f'União: {conjunto_x.union(conjunto_y)}')
+
+# Exercicio 33
+from random import randrange
+dados = []
+for i in range(15):
+    dados.append(randrange(0, 10))
+print(dados)
+
+cont_zeros = 0
+for i in range(len(dados)):
+    if dados[i] == 0:
+        cont_zeros += 1
+print(cont_zeros)
+
+for i in range(cont_zeros):
+    dados.remove(0)
+print(dados)
+
+# Exercicio 34
+lista = []
+entrada = int(input('Digite um número: '))
+lista.append(entrada)
+for i in range(9):
+    while True:
+        entrada = int(input(f'Digite um número: '))
+        if lista.count(entrada) == 0:
+            lista.append(entrada)
+            break
+        print('Valor existente ...')
+print(lista)
+
+# Exercicio 36
+from random import randrange
+
+vetor = []
+for num in range(10):
+    vetor.append(randrange(0, 50))
+vetor.sort()
+print(vetor)
+
+# Exercicio 37
+from random import randrange
+
+vetor = []
+for num in range(11):
+    vetor.append(randrange(0, 50))
+print(vetor)
+
+vetor_crescente = []
+vetor_decrescente = []
+for num in range(len(vetor)):
+    if num < 6:
+        vetor_crescente.append(vetor[num])
+    else:
+        vetor_decrescente.append(vetor[num])
+vetor_crescente.sort()
+print(vetor_crescente)
+vetor_decrescente.sort(reverse=True)
+print(vetor_decrescente)
+"""
---
+++ secao7/exercicio2.py
@@ -0,0 +1,301 @@
+"""
+# Execicio 1
+
+from random import randrange
+
+matriz = [[randrange(0, 50) for numero in range(4)] for valor in range(4)]
+contador = 0
+for linha in range(len(matriz)):
+    for coluna in range(len(matriz)):
+        if matriz[linha][coluna] > 10:
+            contador += 1
+print(matriz)
+print(f'Existem {contador} números maiores que 10')
+
+# Exercicio 2
+
+matriz = [['*' for coluna in range(5)] for linha in range(5)]
+for linha in range(len(matriz)):
+    for coluna in range(len(matriz)):
+        if linha == coluna:
+            matriz[linha][coluna] = 1
+        else:
+            matriz[linha][coluna] = 0
+print(matriz)
+
+# Exercicio 3
+
+matriz = [['*' for coluna in range(4)] for linha in range(4)]
+for linha in range(len(matriz)):
+    for coluna in range(len(matriz)):
+        matriz[linha][coluna] = linha * coluna
+print(matriz)
+
+# Exercicio 4
+from random import randrange
+
+matriz = [[randrange(0, 50) for numero in range(4)] for valor in range(4)]
+maior = 0
+localizacao = list()
+for linha in range(len(matriz)):
+    for coluna in range(len(matriz)):
+        if matriz[linha][coluna] > maior:
+            maior = matriz[linha][coluna]
+            localizacao.clear()
+            localizacao.append((linha, coluna))
+print(matriz)
+print(localizacao)
+
+# Exercicio 5
+from random import randrange
+matriz = [[randrange(0, 50) for coluna in range(5)] for linha in range(5)]
+entrada = randrange(0, 50)
+x = -1
+y = -1
+
+for linha in range(5):
+    for coluna in range(5):
+        print(matriz[linha][coluna], end=' ')
+    print()
+
+print(f'Valor da entrada: {entrada}')
+
+for linha in range(5):
+    for coluna in range(5):
+        if matriz[linha][coluna] == entrada:
+            x = linha
+            y = coluna
+
+if x == -1:
+    print('Não encontrado.')
+else:
+    print(f'Valor encontrado na linha {x} e coluna {y}')
+
+# Exercicio 6
+from random import randrange
+
+matriz1 = [[randrange(0, 100) for coluna in range(4)] for linha in range(4)]
+matriz2 = [[randrange(0, 100) for coluna in range(4)] for linha in range(4)]
+
+print(matriz1)
+print(matriz2)
+
+matriz_maior = [[max(matriz1[linha][coluna], matriz2[linha][coluna])
+                 for coluna in range(4)] for linha in range(4)]
+print(matriz_maior)
+
+# Exercicio 7
+matriz = [['*' for coluna in range(10)] for linha in range(10)]
+
+for linha in range(10):
+    for coluna in range(10):
+        if linha < coluna:
+            matriz[linha][coluna] = (2 * linha) + (7 * coluna) - 2
+        elif linha == coluna:
+            matriz[linha][coluna] = (3 * linha ** 2) - 1
+        else:
+            matriz[linha][coluna] = (4 * linha ** 3) - (5 * coluna ** 2) + 1
+print(matriz)
+
+# Exercicio 8
+from random import randrange
+
+matriz = [[randrange(0, 100) for coluna in range(3)] for linha in range(3)]
+
+soma = 0
+for linha in range(3):
+    for coluna in range(3):
+        if coluna > linha:
+            soma += matriz[linha][coluna]
+print(f'A soma dos valores acima da diagonal principal é {soma}')
+
+# Exercicio 9
+
+from random import randint
+
+matriz = [[randint(0, 50) for coluna in range(3)] for linha in range(3)]
+soma = 0
+for linha in range(len(matriz)):
+    for coluna in range(len(matriz)):
+        if linha > coluna:
+            soma += matriz[linha][coluna]
+
+print(f'A soma dos elementos abaixo da diagonal principal é {soma}')
+
+# Exercicio 11
+
+from random import randint
+
+matriz = [[randint(0, 50) for coluna in range(3)] for linha in range(3)]
+soma = 0
+
+for linha in range(3):
+    for coluna in range(3):
+        if linha + coluna == len(matriz) - 1:
+            soma += matriz[linha][coluna]
+
+print(f'A soma da diagnal secundária é {soma}')
+
+# Exercicio 12
+from random import randint
+
+matriz = [[randint(0, 50) for coluna in range(3)] for linha in range(3)]
+
+transposta = []
+for linha in range(3):
+    transposta.append([matriz[coluna][linha] for coluna in range(3)])
+print()
+
+print(f'Matriz: {matriz}\nTranposta: {transposta}')
+
+# Exercicio 13
+from random import randint
+import numpy as np
+
+ordem = 4
+matriz = [[randint(1, 20) for coluna in range(ordem)] for linha in range(ordem)]
+
+print(np.matrix(matriz))
+
+print()
+for linha in range(ordem):
+    for coluna in range(ordem):
+        if linha < coluna:
+            matriz[linha][coluna] = 0
+
+print(np.matrix(matriz))
+
+# exercicio 14
+from random import randint
+import numpy as np
+
+ordem = 5
+cartela = [[-1 for coluna in range(ordem)] for linha in range(ordem)]
+
+
+def in_matrix(numero, matriz):
+    #verifica se um número está na matriz
+    for i in range(len(matriz[0])):
+        for j in range(len(matriz)):
+            if numero == matriz[i][j]:
+                return true
+    return false
+
+
+for linha in range(ordem):
+    for coluna in range(ordem):
+        while true:
+            numero = randint(0, 99)
+            if not in_matrix(numero, cartela):
+                cartela[linha][coluna] = numero
+                break
+
+print(np.matrix(cartela))
+
+# exercicio 15
+from random import choice
+import numpy as np
+
+opcoes = ['a', 'b', 'c', 'd']
+garabarito = [choice(opcoes) for quantidade in range(10)]
+provas = [[choice(opcoes) for resposta in range(10)] for aluno in range(5)]
+resultado = []
+
+print(f'garabarito: {garabarito}')
+print(f'prova: {np.matrix(provas)}')
+
+soma = 0
+for aluno in range(5):
+    for resposta in range(10):
+        if provas[aluno][resposta] == garabarito[resposta]:
+            soma += 1
+    resultado.append(soma)
+    soma = 0
+
+print(f'resultado: {resultado}')
+
+# Exercicio 16
+from random import choice
+import numpy as np
+
+opcoes = ['a', 'b', 'c', 'd', 'e']
+gabarito = [choice(opcoes) for aluno in range(10)]
+print(gabarito)
+
+provas = [[choice(opcoes) for resposta in range(10)] for aluno in range(3)]
+print(np.matrix(provas))
+
+resultado = []
+soma = 0
+for aluno in range(3):
+    for resposta in range(10):
+        if provas[aluno][resposta] == gabarito[resposta]:
+            soma += 1
+    resultado.append(soma)
+    soma = 0
+
+print(resultado)
+
+# Exericio 17
+from random import randint
+import numpy as np
+
+notas = [[randint(0, 10) for nota in range(3)] for aluno in range(10)]
+print(np.matrix(notas))
+
+pior_nota1 = 0
+pior_nota2 = 0
+pior_nota3 = 0
+for aluno in range(len(notas)):
+    if min(notas[aluno]) == notas[aluno][0]:
+        pior_nota1 += 1
+    elif min(notas[aluno]) == notas[aluno][1]:
+        pior_nota2 += 1
+    elif min(notas[aluno]) == notas[aluno][2]:
+        pior_nota3 += 1
+
+print(f'{pior_nota1} tiveram a pior nota na primeira prova.')
+print(f'{pior_nota2} tiveram a pior nota na segunda prova.')
+print(f'{pior_nota3} tiveram a pior nota na terceira prova.')
+
+# Exercicio 18
+from random import randint
+import numpy as np
+
+ORDEM = 3
+matriz = [[randint(0, 50) for coluna in range(ORDEM)]
+          for linha in range(ORDEM)]
+
+print(np.matrix(matriz))
+
+soma = 0
+resultado = []
+for coluna in range(ORDEM):
+    for linha in range(ORDEM):
+        soma += matriz[linha][coluna]
+    resultado.append(soma)
+    soma = 0
+
+print(f'O resultado da soma das coluna é {resultado}')
+
+# Exercicio 19
+import numpy as np
+from random import randint
+
+matriz = [['*' for coluna in range(4)] for linha in range(5)]
+
+maior_nota = 0
+soma = 0
+for linha in range(len(matriz)):
+    matriz[linha][0] = randint(1, 100)
+    matriz[linha][1] = randint(0, 10)
+    matriz[linha][2] = randint(0, 10)
+    matriz[linha][3] = matriz[linha][1] + matriz[linha][2]
+    soma += matriz[linha][3]
+    if matriz[linha][3] > maior_nota:
+        maior_nota = matriz[linha][3]
+
+print(np.matrix(matriz))
+print(f'A maior nota final é {maior_nota}')
+print(f'A média das notas finais é {soma / len(matriz)}')
+"""
