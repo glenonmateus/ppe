b423bf9fa85a51f4727c6a0cab156ade7b4c88b4
Glenon Mateus <glenonmateus@gmail.com>
2019-04-24 15:04:21 -0300

    add multiderivação indireta

--- secao17/heranca_multipla.py
+++ secao17/heranca_multipla.py
@@ -8,7 +8,6 @@ métodos de todas as classes herdadas
 # OBS: a herança múltipla pode ser feita de duas maneiras:
     - Por multiderivação direta;
     - Por multiderivação indireta;
-"""
 
 # Exemplo 1 - Multiderivação direta
 
@@ -27,3 +26,86 @@ class Base3:
 
 class MultiDerivada(Base1, Base2, Base3):
     pass
+
+
+# Exemplo 2 - Multiderivação indireta
+
+class Base1:
+    pass
+
+
+class Base2(Base1):
+    pass
+
+
+class Base3(Base2):
+    pass
+
+
+class MultiDerivada(Base3):
+    pass
+
+OBS: Não importa se a derivação é direta e indireta. A classe que realizar a
+herança herdará todos os atributos e métodos da super classe.
+"""
+
+
+class Animal:
+
+    def __init__(self, nome):
+        self.__nome = nome
+
+    def cumprimentar(self):
+        return f'Eu sou {self.__nome}'
+
+
+class Aquatico(Animal):
+
+    def __init__(self, nome):
+        super().__init__(nome)
+
+    def nadar(self):
+        return f'{self._Animal__nome} está nadando.'
+
+    def cumprimentar(self):
+        return f'Eu sou {self._Animal__nome} do mar!'
+
+
+class Terrestre(Animal):
+
+    def __init__(self, nome):
+        super().__init__(nome)
+
+    def andar(self):
+        return f'{self._Animal__nome} está andando.'
+
+    def cumprimentar(self):
+        return f'Eu sou {self._Animal__nome} da terra!'
+
+
+class Pinguim(Terrestre, Aquatico):
+
+    def __init__(self, nome):
+        super().__init__(nome)
+
+
+# Testando
+baleia = Aquatico('Wally')
+print(baleia.nadar())
+print(baleia.cumprimentar())
+
+tatu = Terrestre('Xim')
+print(tatu.andar())
+print(tatu.cumprimentar())
+
+tux = Pinguim('Tux')
+print(tux.andar())
+print(tux.nadar())
+print(tux.cumprimentar())  # ??? Method Resolution Order (MRO)
+
+# Objeto é instância de ...
+print(f'Tux é instância de Pinguim? {isinstance(tux, Pinguim)}')
+print(f'Tux é instância de Aquático? {isinstance(tux, Aquatico)}')
+print(f'Tux é instância de Terrestre? {isinstance(tux, Terrestre)}')
+print(f'Tux é instância de Terrestre? {isinstance(tux, Terrestre)}')
+print(f'Tux é instância de object? {isinstance(tux, object)}')
